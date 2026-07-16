f708cea04ef504a24a2e830183154f0cd91f1475
Aaditya Maindarkar <147494839+Aaditya8459@users.noreply.github.com>
2026-07-16 23:52:20 +0530

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/MyPortfolio",     // repo name
-  assetPrefix: "/MyPortfolio/", // repo name
-  images: { unoptimized: true },
-};
-
-export default nextConfig;
